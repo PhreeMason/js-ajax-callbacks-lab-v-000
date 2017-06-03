06d4ff1e6fddfc9226e83c414f8ff5eafaf5621d
Omar Mason <mr.omarmason@gmail.com>
2017-06-03 08:37:29 +0000

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ function handlebarsSetup() {
 $(document).ready(function (){
   handlebarsSetup()
 });
+
+function searchRepositories() {
+  var query = $('#searchTerms').value;
+  var url  = `https://api.github.com/search/repositories/?q=${query}`;
+  $.get(url, showRepositories(data)).fail(displayError(error));
+  debugger
+}
+
+function showRepositories(data) {
+  repos = data.items
+  template = Handlebars.compile($('#repository-template'))
+  repoList = template(repos)
+  $('#results').html(repoList)
+}
+
+
+function displayError(error) {
+  $('#errors').html(`error: ${error}`)
+}
